88527a6586fb2b7fda8832a1842e36bc4cb96736
Mohamed El attar <mohamadelattar99@gmail.com>
2024-10-07 23:11:16 +0100

    "Retroden Project"

---
+++ src/main/java/com/api/retroden/RetrodenApplication.java
@@ -0,0 +1,13 @@
+package com.api.retroden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrodenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetrodenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/retroden/RetrodenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.retroden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrodenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
