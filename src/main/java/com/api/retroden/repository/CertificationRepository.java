44aaf8a3c759e1b938bbd423e4e13d030f70e6a0
Mohamed El attar <mohamadelattar99@gmail.com>
2024-10-14 00:13:24 +0100

    "Setup Certification Service and Edit Certification Repository"

--- src/main/java/com/api/retroden/repository/CertificationRepository.java
+++ src/main/java/com/api/retroden/repository/CertificationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CertificationRepository extends JpaRepository<Long, Certification> {
+public interface CertificationRepository extends JpaRepository<Certification, Long> {
 }
---
+++ src/main/java/com/api/retroden/service/CertificationService.java
@@ -0,0 +1,43 @@
+package com.api.retroden.service;
+
+import com.api.retroden.model.Certification;
+import com.api.retroden.repository.CertificationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CertificationService {
+    private final CertificationRepository certificationRepository;
+
+    public CertificationService(CertificationRepository certificationRepository) {
+        this.certificationRepository = certificationRepository;
+    }
+    public Certification create(Certification certification){
+        return certificationRepository.save(certification);
+    }
+    public List<Certification> findAll(){
+        return certificationRepository.findAll();
+    }
+
+    public Optional<Certification> findById(Long id){
+        return certificationRepository.findById(id);
+    }
+    public Certification update(Long id, Certification certification){
+        Optional<Certification> certifOptional = certificationRepository.findById(id);
+        if(certifOptional.isPresent()){
+            Certification existingCertification = certifOptional.get();
+            existingCertification.setName(certification.getName());
+            existingCertification.setData(certification.getData());
+            return certificationRepository.save(existingCertification);
+        } else {
+            throw new RuntimeException("Certification not found with id: " + id);
+        }
+    }
+    public void delete(Long id){
+        certificationRepository.deleteById(id);
+    }
+
+
+}
