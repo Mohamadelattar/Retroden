83b376648f85122ab673c5d8c366225ad37f1112
Mohamed El attar <mohamadelattar99@gmail.com>
2025-03-12 01:22:02 +0000

    "Refqctoring SkillService and add SkillController"

---
+++ src/main/java/com/api/retroden/controller/SkillController.java
@@ -0,0 +1,43 @@
+package com.api.retroden.controller;
+
+import com.api.retroden.dto.request.SkillRequest;
+import com.api.retroden.dto.response.SkillResponse;
+import com.api.retroden.service.SkillService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/skills")
+public class SkillController {
+    private final SkillService skillService;
+
+    public SkillController(SkillService skillService) {
+        this.skillService = skillService;
+    }
+
+    @GetMapping
+    public ResponseEntity<SkillResponse> findById(@RequestParam Long id) {
+        return ResponseEntity.ok(skillService.findById(id));
+    }
+    @GetMapping
+    public ResponseEntity<List<SkillResponse>> findAll() {
+        return ResponseEntity.ok(skillService.findAll());
+    }
+    @PostMapping
+    public ResponseEntity<SkillResponse> save(@RequestBody @Valid  SkillRequest skillRequest) {
+        this.skillService.create(skillRequest);
+        return ResponseEntity.accepted().body("Skill created");
+    }
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody @Valid SkillRequest skillRequest) {
+        return ResponseEntity.ok(skillService.update(skillRequest));
+    }
+    @DeleteMapping
+    public void delete(@RequestParam Long id) {
+        this.skillService.delete(id);
+    }
+
+}
--- src/main/java/com/api/retroden/service/SkillService.java
+++ src/main/java/com/api/retroden/service/SkillService.java
@@ -1,5 +1,8 @@
 package com.api.retroden.service;
 
+import com.api.retroden.dto.mapper.SkillMapper;
+import com.api.retroden.dto.request.SkillRequest;
+import com.api.retroden.dto.response.SkillResponse;
 import com.api.retroden.model.Skill;
 import com.api.retroden.repository.SkillRepository;
 import org.springframework.stereotype.Service;
@@ -10,32 +13,38 @@ import java.util.Optional;
 @Service
 public class SkillService {
     private final SkillRepository skillRepository;
+    private final SkillMapper skillMapper;
 
-    public SkillService(SkillRepository skillRepository) {
+    public SkillService(SkillRepository skillRepository, SkillMapper skillMapper) {
         this.skillRepository = skillRepository;
+        this.skillMapper = skillMapper;
     }
-    public List<Skill> findAll(){
-        return skillRepository.findAll();
+    public List<SkillResponse> findAll(){
+        return skillRepository.findAll()
+                .stream()
+                .map(skillMapper::toSkillResponse)
+                .toList();
     }
 
-    public Skill findById(Long id){
+    public SkillResponse findById(Long id){
         return  skillRepository.findById(id)
+                .map(skillMapper::toSkillResponse)
                 .orElseThrow(() -> new RuntimeException("Skill not found with id: " + id));
     }
     public Skill create (Skill skill){
         return skillRepository.save(skill);
     }
 
-    public Skill update (Long id, Skill skill){
-        Optional<Skill> skillOptional = skillRepository.findById(id);
-        if(skillOptional.isPresent()){
-            Skill existingSkill = skillOptional.get();
-            existingSkill.setSkillName(skill.getSkillName());
-            existingSkill.setProfessional(skill.getProfessional());
-         return skillRepository.save(existingSkill);
-        } else {
-            throw new RuntimeException("Skill not found with id: " + id);
-        }
+    public SkillResponse update (SkillRequest skillRequest){
+        return skillMapper.toSkillResponse(
+                skillRepository.findById(skillRequest.id())
+                        .map(existingSkill -> {
+                            Optional.ofNullable(skillRequest.name()).ifPresent(existingSkill::setSkillName);
+                            return skillRepository.save(existingSkill);
+                        })
+                        .orElseThrow(() -> new RuntimeException("Skill not found with id " + skillRequest.id())));
+
+
     }
     public void delete (Long id){
         skillRepository.deleteById(id);
